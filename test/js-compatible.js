dd5cb65a896aebaad8785f749a386a3f70575565
DoubleU23 <sf@tailored-digital.com>
2015-12-04 12:24:03 +0100

    stuff

---
+++ test/js-compatible.js
@@ -0,0 +1,33 @@
+var should = require("should");
+var path = require("path");
+var fileLoader = require("../");
+
+function run(resourcePath, query, content) {
+	content = content || new Buffer("1234");
+	var result = null;
+	var context = {
+		resourcePath: resourcePath,
+		query: "?" + query,
+		options: {
+			context: ""
+		},
+		emitFile: function(url, content2) {
+			content2.should.be.eql(content);
+			result = url;
+		}
+	};
+	fileLoader.call(context, content);
+	return result;
+}
+
+function test(excepted, resourcePath, query, content) {
+	run(resourcePath, query, content).should.be.eql(excepted);
+}
+
+describe("correct-filename", function() {
+	it("should return valid js", function() {
+
+		test("{ mui_button { 'font-size': '20px' } }", "./test.css", "");
+
+	});
+});
