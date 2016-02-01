aca6235c8a40df951fcf8e5cbee70f13c179d415
DoubleU23 <sf@tailored-digital.com>
2016-02-01 15:23:32 +0100

    stable version

--- index.js
+++ index.js
@@ -1,59 +1,52 @@
 /*
 	MIT License http://www.opensource.org/licenses/mit-license.php
-	Author Tobias Koppers @sokra
+	Author - Stefan Friedl @DoubleU23
 */
-var loaderUtils = require('loader-utils'),
-	path = require('path'),
-	nativeCss = require('native-css')
-	fs = require('fs'),
-	childProcess = require('child_process');
-
-module.exports = function(content) {
-
-	this.cacheable && this.cacheable();
-	if(!this.emitFile) throw new Error('emitFile is required from module system');
-	// parameters
-	var query = loaderUtils.parseQuery(this.query);
-	var url = loaderUtils.interpolateName(this, query.name || '[hash].[ext]', {
-		context: query.context || this.options.context,
-		content: content,
-		regExp: query.regExp
-	});
-
-// var path 			= 'tmp/';
-var that 			= this
-,	path 			= __dirname + '/tmp/'
-,	newUrl 		= path+url
-,	compiled, compiledJson, exportString
+var loaderUtils = require('loader-utils')
+,	path = require('path')
+,	nativeCss = require('../native-css')
+,	fetchUrl = require('fetch').fetchUrl
+,	fs = require('fs')
+,	childProcess = require('child_process')
 ;
-
-var callback 	= function() {
-	compiled 		= nativeCss.convert( 'node_modules/cssobjects-loader/tmp/'+url );
-	compiledJson = JSON.stringify(compiled);
-	that.emitFile(url, compiledJson);
-	exportString = ''+compiledJson;
-}
-
-fs.readdir(path, function(err) {
-	if (err) // dir not found
-		throw err;
-	console.log('tmp dir found' );
-
-	fs.writeFile(newUrl, content, 'utf8', function(err) {
-		console.log('writeFile newUrl', newUrl,  err);
-		callback();
-	});
-});
-
-compiled 		= nativeCss.convert( 'node_modules/cssobjects-loader/tmp/'+url );
-compiledJson = JSON.stringify(compiled);
-that.emitFile(url, compiledJson);
-exportString = ''+compiledJson;
-
-this.emitFile(url, compiledJson);
-// // newUrlRealtive 	= '../../..'+url;
-return 'module.exports =  ' + exportString + ';';
-
-	// return 'module.exports =  __webpack_public_path__ + ' + JSON.stringify(url) + ';';
+module.exports = function(content) {
+	this.cacheable && this.cacheable();
+	if(!this.emitFile)
+		throw new Error('emitFile is required from module system');
+	var callback 	= this.async()
+	,	query 		= loaderUtils.parseQuery(this.query)
+	,	url 			= loaderUtils.interpolateName(this, query.name || '[hash].[ext]', {
+			context: query.context || this.options.context,
+			content: content,
+			regExp: query.regExp
+		})
+	,	path 	= this._compiler.options.output.path + '/' + url
+	,	result 	=  fs.writeFile(path, content, function() {
+			var result 		= nativeCss.convert(path, true)
+			,	returnVal 	= 'module.exports =  ' + JSON.stringify(result);
+			callback(null, returnVal);
+		})
+	;
+	/**
+	 *	Async variant per emiFile's content
+	 * 
+	 * 	emitFile writes later (fileWrites.push)
+	 * 	if there would be a callback to emitFile, this should be run.
+	 *
+	this.emitFile(url, content);
+	var emitFileCallback = function() {
+		var url = this._compiler.options.output.path + 'url';
+		compiled = nativeCss.convertAsync(url, true )
+					.then(function(result) {
+						console.log('result', result);
+						callback(null,
+							'module.exports =  ' + JSON.stringify(result)
+						);
+					})
+					.catch(function(err) {
+						callback(err);
+					});
+	};
+	 */
 }
 module.exports.raw = true;
