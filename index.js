a8cfc33ba9434ec12b258df0e4b8a785827a1dc2
DoubleU23 <sf@tailored-digital.com>
2016-02-26 12:52:52 +0100

    removed writeFile -> use content-Buffer/String

--- index.js
+++ index.js
@@ -1,37 +1,25 @@
 /*
-    MIT License http://www.opensource.org/licenses/mit-license.php
-    Author - Stefan Friedl @DoubleU23
+	MIT License http://www.opensource.org/licenses/mit-license.php
+	Author - Stefan Friedl @DoubleU23
 */
-var loaderUtils     = require('loader-utils')
-,   path            = require('path')
-,   nativeCss       = require('../native-css')
-,   fs              = require('fs')
+var loaderUtils 		= require('loader-utils')
+,	path 			= require('path')
+,	nativeCss 		= require('../native-css')
+,	fs 				= require('fs')
 ;
 module.exports = function(content) {
-    // questionable
-    // this.cacheable && this.cacheable();
-    if(!this.emitFile) throw new Error('emitFile is required from module system');
-    var callback    = this.async()
-    ,   query       = loaderUtils.parseQuery(this.query)
-    ,   url         = loaderUtils.interpolateName(this, query.name || '[hash].[ext]', {
-            context: query.context || this.options.context,
-            content: content,
-            regExp: query.regExp
-        })
-    ,   path        = this._compiler.options.output.path + '/' + url
-    /**
-     *  'ugly' writeFile solution
-     *  Async variant per emiFile's URL not possible
-     *  because emitFile writes later (fileWrites.push)
-     *
-     *  wait until nativeCss accepts buffers
-     */
-    ,   result      =  fs.writeFile(path, content, function(err) {
-            if (err) throw new Error('[cssobjects-loader->writefile] Error:', err);
-            var result      = nativeCss.convert(path, true)
-            ,   returnVal   = 'module.exports =  ' + JSON.stringify(result);
-            callback(null, returnVal);
-        })
-    ;
+	// questionable - i dont like to cache styles as its dev-madness
+	// this.cacheable && this.cacheable();
+	if(!this.emitFile)
+		throw new Error('emitFile is required from module system');
+	var query 		= loaderUtils.parseQuery(this.query)
+	,	url 			= loaderUtils.interpolateName(this, query.name || '[hash].[ext]', {
+			context: query.context || this.options.context,
+			content: content,
+			regExp: query.regExp
+		})
+	,	result 	= nativeCss.convert(content)
+	;
+	return 'module.exports =  ' + JSON.stringify(result);
 }
 module.exports.raw = true;
